19c9dc7e6c8b502c054378413e0c23c2d621bd8a
cayman444 <artursavinX@yandex.ru>
2025-09-27 19:52:06 +0300

    chore: movie-app init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SearchOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <Button type="primary" icon={<SearchOutlined />}>
+      Search
+    </Button>
+  );
+}
+
+export default App;
