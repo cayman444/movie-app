fe9c5ad7d7468b4faf734a26e8178363b7ad8923
cayman444 <artursavinX@yandex.ru>
2025-10-07 13:26:29 +0300

    feat: add select order, year

--- src/features/movies/components/MoviesFilters.tsx
+++ src/features/movies/components/MoviesFilters.tsx
@@ -11,7 +11,21 @@ export const MoviesFilters: FC<ComponentProps<'div'>> = ({ className }) => {
     <div className={clsx(className, 'flex gap-2')}>
       <Select
         suffixIcon={<DownOutlined style={{ color: '#ffffff73' }} />}
+        placeholder="Сортировка"
+        defaultValue="По оценкам"
+        allowClear
+        style={{ width: 200 }}
+        loading={isFetching}
+        options={[
+          { label: 'По оценкам', value: 'По оценкам' },
+          { label: 'По рейтингу', value: 'По рейтингу' },
+          { label: 'По году', value: 'По году' },
+        ]}
+      />
+      <Select
+        suffixIcon={<DownOutlined style={{ color: '#ffffff73' }} />}
         placeholder="Страна"
+        allowClear
         style={{ width: 200 }}
         loading={isFetching}
         options={filters?.countries}
@@ -19,9 +33,21 @@ export const MoviesFilters: FC<ComponentProps<'div'>> = ({ className }) => {
       <Select
         suffixIcon={<DownOutlined style={{ color: '#ffffff73' }} />}
         placeholder="Жанр"
+        allowClear
         style={{ width: 200 }}
         loading={isFetching}
         options={filters?.genres}
+      />
+      <Select
+        suffixIcon={<DownOutlined style={{ color: '#ffffff73' }} />}
+        placeholder="Год"
+        allowClear
+        style={{ width: 200 }}
+        loading={isFetching}
+        options={Array.from({ length: 60 }, (_, ind) => ({
+          label: new Date().getFullYear() - ind,
+          value: new Date().getFullYear() - ind,
+        }))}
       />
     </div>
   );
