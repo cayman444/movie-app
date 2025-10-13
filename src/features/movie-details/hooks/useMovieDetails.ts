6731d5fe4db3bf84c2ae4b0c4b3bb78ed0d6637b
cayman444 <artursavinX@yandex.ru>
2025-10-13 11:46:51 +0300

    feat: add scroll to top when change movie id

--- src/features/movie-details/hooks/useMovieDetails.ts
+++ src/features/movie-details/hooks/useMovieDetails.ts
@@ -4,7 +4,7 @@ import {
   useGetSequelsPrequelsQuery,
   useGetSimilarMoviesQuery,
 } from '@/shared/api/endpoints';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const useMovieDetails = () => {
@@ -20,12 +20,16 @@ export const useMovieDetails = () => {
   const { currentData: sequelsPrequels, isFetching: sequelsPrequelsLoading } =
     useGetSequelsPrequelsQuery({ id: +movieId }, { skip: !movieId });
 
-  const { currentData: similarMovies, isFetching: SimilarMoviesLoading } =
+  const { data: similarMovies, isFetching: SimilarMoviesLoading } =
     useGetSimilarMoviesQuery({ id: +movieId }, { skip: !movieId });
 
   const onChangeSelectedPlayerIndex = (value: string) => {
     setSelectedPlayerIndex(+value);
   };
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [id]);
 
   return {
     selectedPlayerIndex,
